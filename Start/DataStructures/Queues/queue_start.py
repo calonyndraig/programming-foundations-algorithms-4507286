21f9bb9c85dd205d3f247fb50e9652f275548eba
calonyndraig <calonnicole.guntercox@gmail.com>
2025-01-17 15:21:30 -0600

    commit

--- Start/DataStructures/Queues/queue_start.py
+++ Start/DataStructures/Queues/queue_start.py
@@ -1,14 +1,23 @@
 # Example file for Programming Foundations: Algorithms by Joe Marini
 # try out the Python queue functions
-
+from collections import deque
 
 # TODO: create a new empty deque object that will function as a queue
-
+queue = deque()
 
 # TODO: add some items to the queue
-
+queue.append(1)
+queue.append(2)
+queue.append(3)
+queue.append(4)
+queue.append(5)
 
 # TODO: print the queue contents
-
+print(queue)
 
 # TODO: pop an item off the front of the queue
+x = queue.popleft()
+print(x)
+y = queue.pop()
+print(y)
+print(queue)
--- Start/Introduction/gcd_start.py
+++ Start/Introduction/gcd_start.py
@@ -4,9 +4,13 @@
 
 
 def gcd(a, b):
-    pass
-
+    while b != 0: # while b is not zero
+        temp = a # sets current a to a temp variable
+        a = b # sets a to b (since b will be the next a we try or the remainder)
+        b = temp % b # sets the remainder to b
+    return a
 
 # try out the function with a few examples
 print(gcd(60, 96))  # should be 12
 print(gcd(20, 8))   # should be 4
+print(gcd(120,25)) # should be 5
